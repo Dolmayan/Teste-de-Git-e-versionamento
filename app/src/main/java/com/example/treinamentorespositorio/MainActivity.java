806ba06e7fc16aa45d7e33448d1d231cf80515bc
Nicolas <nicolasrochabr@outlook.com>
2020-11-13 15:42:31 -0300

    texte Branch 2

--- app/src/main/java/com/example/treinamentorespositorio/MainActivity.java
+++ app/src/main/java/com/example/treinamentorespositorio/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.treinamentorespositorio;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private Button texte;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        func();
+    }
+
+    public void func(){
+        texte = findViewById(R.id.button21);
+        texte.setOnClickListener(v -> Toast.makeText(MainActivity.this, "XABLAU", Toast.LENGTH_SHORT).show());
     }
 }
